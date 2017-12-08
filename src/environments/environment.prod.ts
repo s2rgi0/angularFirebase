d835ab06afb2975bb7f6576cfbc1595531668e81
sergio <tlakolu@gmail.com>
2017-12-08 03:12:02 -0600

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+  	apiKey: "AIzaSyBAjevU-2STPazyVQE-KtWccVOt7VFO2dM",
+    authDomain: "fs1prod-86030.firebaseapp.com",
+    databaseURL: "https://fs1prod-86030.firebaseio.com",
+    projectId: "fs1prod-86030",
+    storageBucket: "fs1prod-86030.appspot.com",
+    messagingSenderId: "943988290318"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+  	apiKey: "AIzaSyBAjevU-2STPazyVQE-KtWccVOt7VFO2dM",
+    authDomain: "fs1prod-86030.firebaseapp.com",
+    databaseURL: "https://fs1prod-86030.firebaseio.com",
+    projectId: "fs1prod-86030",
+    storageBucket: "fs1prod-86030.appspot.com",
+    messagingSenderId: "943988290318"
+  }
 };
